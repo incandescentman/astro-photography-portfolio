9dcd409a3c2c0dbb6001f084bf19e5703ea5f1fc
Jay Dixit <jaydixit.work@gmail.com>
2025-09-05 01:39:29 -0600

    weird gaps; broken state

--- src/data/homepage-images.js
+++ src/data/homepage-images.js
@@ -15,119 +15,141 @@
 // - If not provided, the filename will be used (with dashes/underscores converted to spaces)
 
 export const images = [
-
-{
+  // First row - Start with xlportrait on left, fill with portraits
+  {
     order: 1,
+    filename: 'vanessa-kirby.jpg',
+    caption: 'Vanessa Kirby',
+    size: 'xlportrait'
+  },
+  {
+    order: 2,
     filename: 'dixit_nobel-physics.jpg',
     size: 'portrait',
     caption: 'Nobel Prize in Physics Ceremony'
   },
   {
+    order: 3,
     filename: 'judd-apatow.jpg',
     caption: 'Judd Apatow',
-    size: 'portrait',
-    order: 2
+    size: 'portrait'
   },
-
-{
-    order: 3, 
-    filename: 'vanessa-kirby.jpg',
-    caption: 'Vanessa Kirby',
-    size: 'xlportrait' 
-  },
-{
-    order:4,
+  {
+    order: 4,
     filename: 'jeremy.jpg',
     caption: 'Jeremy Strong',
     size: 'portrait'
   },
+  // Continue filling the grid - add portrait to complete the row under xlportrait
   {
-      order:5,
+    order: 5,
     filename: 'Lisa_Gilroy_at_SXSW_in_2025-1.jpg',
     caption: 'Lisa Gilroy at SXSW in 2025',
-    size: 'portrait' // Portrait, not landscape
+    size: 'portrait'
   },
   {
+    order: 6,
     filename: 'jeremy-3.jpg',
     caption: 'Jeremy Strong',
     size: 'portrait'
   },
   {
+    order: 7,
     filename: 'conan.jpg',
     caption: "Conan O'Brien",
     size: 'portrait'
   },
+  // Add landscape items - they span 2 columns
   {
-    filename: 'jeremy-6.jpg',
-    caption: 'Jeremy',
-    size: 'portrait' // Fill the gap before the wide_tall
-  },
-  {
-    filename: 'jay-dixit_red-carpet_01130.jpg',
-    caption: 'Jay Dixit on the Red Carpet',
-    size: 'xlportrait' // FEATURE IMAGE - now no gap to its left
-  },
-  {
+    order: 8,
     filename: 'jeremy-11.jpg',
     caption: 'Jeremy',
-    size: 'landscape' // LANDSCAPE
+    size: 'landscape'
   },
   {
+    order: 9,
     filename: 'vinod.jpg',
     caption: 'Vinod',
-    size: 'landscape' // LANDSCAPE
+    size: 'landscape'
   },
   {
+    order: 10,
+    filename: 'jeremy-6.jpg',
+    caption: 'Jeremy',
+    size: 'portrait'
+  },
+  // Second xlportrait with proper surrounding
+  {
+    order: 11,
+    filename: 'jay-dixit_red-carpet_01130.jpg',
+    caption: 'Jay Dixit on the Red Carpet',
+    size: 'xlportrait'
+  },
+  {
+    order: 12,
     filename: 'jay-dixit_red-carpet_05923-1.jpg',
     caption: 'Jay Dixit Red Carpet',
     size: 'portrait'
   },
   {
+    order: 13,
     filename: 'jeremy-4.jpg',
     caption: 'Jeremy',
     size: 'portrait'
   },
   {
+    order: 14,
     filename: 'vinod-4.jpg',
     caption: 'Vinod',
     size: 'portrait'
   },
+  // Continue filling - portrait under the xlportrait
   {
+    order: 15,
     filename: 'jeremy-9.jpg',
     caption: 'Jeremy Strong',
     size: 'portrait'
   },
   {
+    order: 16,
     filename: 'vinod-3.jpg',
     caption: 'Vinod',
     size: 'portrait'
   },
   {
+    order: 17,
     filename: 'jay-dixit_red-carpet_05456.jpg',
     caption: 'Jay Dixit Red Carpet',
     size: 'portrait'
   },
+  // Add landscape to create better flow
   {
-    filename: 'Gabriel_LaBelle_at_the_2024_Toronto_International_Film_Festival_03_cropped.jpg',
-    caption: 'Gabriel LaBelle at the 2024 Toronto International Film Festival',
-    size: 'portrait'
-  },
-  {
-    filename: 'Jason_Collett_at_Ottawa_Jazz_Festival_in_2025_-_3.jpg',
-    caption: 'Jason Collett at Ottawa Jazz Festival in 2025',
-    size: 'portrait'
-  },
-  {
-    filename: 'jeremy-7.jpg',
-    caption: 'Jeremy Strong',
-    size: 'xlportrait'
-  },
-  {
+    order: 18,
     filename: 'jay-dixit_red-carpet_08042.jpg',
     caption: 'Jay Dixit Red Carpet',
     size: 'landscape'
   },
   {
+    order: 19,
+    filename: 'Gabriel_LaBelle_at_the_2024_Toronto_International_Film_Festival_03_cropped.jpg',
+    caption: 'Gabriel LaBelle at the 2024 Toronto International Film Festival',
+    size: 'portrait'
+  },
+  {
+    order: 20,
+    filename: 'Jason_Collett_at_Ottawa_Jazz_Festival_in_2025_-_3.jpg',
+    caption: 'Jason Collett at Ottawa Jazz Festival in 2025',
+    size: 'portrait'
+  },
+  // Third xlportrait at the end
+  {
+    order: 21,
+    filename: 'jeremy-7.jpg',
+    caption: 'Jeremy Strong',
+    size: 'xlportrait'
+  },
+  {
+    order: 22,
     filename: 'jeremy-2.jpg',
     caption: 'Jeremy Strong',
     size: 'portrait'
