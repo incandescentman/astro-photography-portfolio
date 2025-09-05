1b866160ddfc98f896a9b69a3b6a7c5c7b809f73
Jay Dixit <jaydixit.work@gmail.com>
2025-09-05 01:53:40 -0600

    Update homepage-images.js

--- src/data/homepage-images.js
+++ src/data/homepage-images.js
@@ -18,114 +18,114 @@ export const images = [
   {
     order: 1,
     filename: 'dixit_nobel-physics.jpg',
-    size: 'portrait',
+    size: 'portrait', // 2048x2560 (0.8 ratio - portrait)
     caption: 'Nobel Prize in Physics Ceremony'
   },
   {
     order: 2,
     filename: 'judd-apatow.jpg',
-    size: 'portrait',
+    size: 'portrait', // 1519x2560 (0.59 ratio - portrait)
     caption: 'Judd Apatow'
   },
   {
     order: 3,
     filename: 'vanessa-kirby.jpg',
-    size: 'xlportrait', // FEATURE IMAGE
+    size: 'xlportrait', // 1978x2560 (0.77 ratio - portrait, featured)
     caption: 'Vanessa Kirby'
   },
   {
     filename: 'jeremy.jpg',
-    size: 'portrait',
+    size: 'portrait', // 2048x2560 (0.8 ratio - portrait)
     caption: 'Jeremy Strong'
   },
   {
     filename: 'Lisa_Gilroy_at_SXSW_in_2025-1.jpg',
-    size: 'portrait',
+    size: 'portrait', // 1707x2560 (0.67 ratio - portrait)
     caption: 'Lisa Gilroy at SXSW in 2025'
   },
   {
     filename: 'jeremy-3.jpg',
-    size: 'portrait',
+    size: 'portrait', // 2048x2560 (0.8 ratio - portrait)
     caption: 'Jeremy Strong'
   },
   {
     filename: 'conan.jpg',
-    size: 'portrait',
+    size: 'portrait', // 1707x2560 (0.67 ratio - portrait)
     caption: "Conan O'Brien"
   },
   {
     filename: 'jeremy-6.jpg',
-    size: 'portrait',
+    size: 'portrait', // 1707x2560 (0.67 ratio - portrait)
     caption: 'Jeremy'
   },
   {
     filename: 'jay-dixit_red-carpet_01130.jpg',
-    size: 'xlportrait', // FEATURE IMAGE
+    size: 'xlportrait', // 1707x2560 (0.67 ratio - portrait, featured)
     caption: 'Jay Dixit on the Red Carpet'
   },
   {
     filename: 'jeremy-11.jpg',
-    size: 'landscape',
+    size: 'landscape', // 2560x1707 (1.5 ratio - landscape)
     caption: 'Jeremy'
   },
   {
     filename: 'vinod.jpg',
-    size: 'landscape',
+    size: 'landscape', // 2560x1707 (1.5 ratio - landscape)
     caption: 'Vinod'
   },
   {
     filename: 'jay-dixit_red-carpet_05923-1.jpg',
-    size: 'portrait',
+    size: 'portrait', // 1707x2560 (0.67 ratio - portrait)
     caption: 'Jay Dixit Red Carpet'
   },
   {
     filename: 'jeremy-4.jpg',
-    size: 'portrait',
+    size: 'portrait', // 2266x2560 (0.89 ratio - portrait)
     caption: 'Jeremy'
   },
   {
     filename: 'vinod-4.jpg',
-    size: 'portrait',
+    size: 'portrait', // 1707x2560 (0.67 ratio - portrait)
     caption: 'Vinod'
   },
   {
     filename: 'jeremy-9.jpg',
-    size: 'portrait',
+    size: 'portrait', // 1707x2560 (0.67 ratio - portrait)
     caption: 'Jeremy Strong'
   },
   {
     filename: 'vinod-3.jpg',
-    size: 'portrait',
+    size: 'portrait', // 1707x2560 (0.67 ratio - portrait, NOT landscape)
     caption: 'Vinod'
   },
   {
     filename: 'jay-dixit_red-carpet_05456.jpg',
-    size: 'portrait',
+    size: 'portrait', // 2048x2560 (0.8 ratio - portrait)
     caption: 'Jay Dixit Red Carpet'
   },
   {
     filename: 'Gabriel_LaBelle_at_the_2024_Toronto_International_Film_Festival_03_cropped.jpg',
-    size: 'portrait',
+    size: 'portrait', // 1583x2491 (0.64 ratio - portrait)
     caption: 'Gabriel LaBelle at the 2024 Toronto International Film Festival'
   },
   {
     filename: 'Jason_Collett_at_Ottawa_Jazz_Festival_in_2025_-_3.jpg',
-    size: 'landscape',
+    size: 'portrait', // 1706x2560 (0.67 ratio - portrait, NOT landscape)
     caption: 'Jason Collett at Ottawa Jazz Festival in 2025'
   },
   {
     filename: 'jeremy-7.jpg',
-    size: 'xlportrait',
+    size: 'xlportrait', // 1707x2560 (0.67 ratio - portrait, featured)
     caption: 'Jeremy Strong'
   },
   {
     filename: 'jay-dixit_red-carpet_08042.jpg',
-    size: 'portrait',
+    size: 'landscape', // 2560x1707 (1.5 ratio - landscape)
     caption: 'Jay Dixit Red Carpet'
   },
   {
     filename: 'jeremy-2.jpg',
-    size: 'portrait',
+    size: 'portrait', // 2048x2560 (0.8 ratio - portrait)
     caption: 'Jeremy Strong'
   }
 ];
